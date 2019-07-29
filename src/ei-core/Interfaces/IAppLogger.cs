6aa8ea6cc3b27b01566f181b50848a0f63129bbe
Lesair Valmont <ixra.net@gmail.com>
2019-07-29 01:18:30 -0500

    [AB#137] Added logger interface.

---
+++ src/ei-core/Interfaces/IAppLogger.cs
@@ -0,0 +1,13 @@
+﻿namespace ei_core.Interfaces
+{
+    // TODO: Make use of the generic type of this logger class.
+    /// <summary>
+    /// This type eliminates the need to depend directly on the ASP.NET Core logging types.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IAppLogger<T>
+    {
+        void LogInformation(string message, params object[] args);
+        void LogWarning(string message, params object[] args);
+    }
+}
