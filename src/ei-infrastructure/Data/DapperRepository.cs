78b6d6139e7c2255793e2eebab9bcc54835e3e6f
Lesair Valmont <ixra.net@gmail.com>
2019-07-29 02:11:35 -0500

    [AB#137] Dapper repo added.

---
+++ src/ei-infrastructure/Data/DapperRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using ei_core.Entities;
+using ei_core.Interfaces;
+
+namespace ei_infrastructure.Data
+{
+    public class DapperRepository<T> : IRepository<T> where T : BaseEntity, IIdentifiable
+    {
+        public Task<T> FindByIdAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
