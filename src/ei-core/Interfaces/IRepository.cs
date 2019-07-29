2356fee1d733a787e35581a3820a722a7872c76e
Lesair Valmont <ixra.net@gmail.com>
2019-07-29 01:35:17 -0500

    [AB#137] Initial IRepository interface definition.
    Generic, with the constraint that the type used as argument for the generic should be a BaseEntity (DDD IIdentifiable).

---
+++ src/ei-core/Interfaces/IRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using ei_core.Entities;
+
+namespace ei_core.Interfaces
+{
+    public interface IRepository<T> where T : BaseEntity, IIdentifiable
+    {
+        Task<T> GetByIdAsync(int id);
+    }
+}
