17d52db1dc15839183adfcee834f48a17fb5c2d1
Lesair Valmont <ixra.net@gmail.com>
2019-09-24 21:46:59 -0500

    [AB#178] RgbColors value object.

---
+++ src/ei-core/Entities/WireColorAggregate/RgbColors.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+
+namespace ei_core.Entities.WireColorAggregate
+{
+    public struct RgbColors
+    {
+        public Color[] Colors { get; }
+
+        public RgbColors(Color color1) : this()
+        {
+            Colors = new Color[1];
+            Colors[0] = color1;
+        }
+
+        public RgbColors(Color color1, Color color2)
+        {
+            Colors = new Color[2];
+            Colors[0] = color1;
+            Colors[1] = color2;
+        }
+    }
+}
