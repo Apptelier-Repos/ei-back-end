ed817fb1a8a925974e76b7d3744538705014021d
Lesair Valmont <ixra.net@gmail.com>
2019-09-24 21:47:19 -0500

    [AB#178] WireColor root aggregate.

---
+++ src/ei-core/Entities/WireColorAggregate/WireColor.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+
+namespace ei_core.Entities.WireColorAggregate
+{
+    public sealed class WireColor : BaseEntity
+    {
+        public string Code { get; }
+        public string Name { get; }
+        public string TranslatedName { get; }
+        public RgbColors Colors { get; }
+
+        public WireColor(int id, string code, string name, string translatedName, RgbColors colors) : base(id)
+        {
+            Code = code;
+            Name = name;
+            TranslatedName = translatedName;
+            Colors = colors;
+        }
+    }
+}
