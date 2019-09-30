ef3f8868ffc7c7739e1eb763f105c2dcb147afc5
Lesair Valmont <ixra.net@gmail.com>
2019-09-30 18:16:31 -0500

    [AB195] GetAllWireColors command.

---
+++ src/ei-infrastructure/Data/POCOs/WireColor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Dapper.Contrib.Extensions;
+
+namespace ei_infrastructure.Data.POCOs
+{
+    public class WireColor
+    {
+        [Key] public int Id { get; set; }
+
+        [Computed] public DateTime CreationDate { get; set; }
+
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string TranslatedName { get; set; }
+        public string BaseColor { get; set; }
+        public string StripeColor { get; set; }
+    }
+}
---
+++ src/ei-infrastructure/Data/Queries/GetAllWireColors.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using Dapper.Contrib.Extensions;
+using ei_core.Entities.WireColorAggregate;
+using MediatR;
+
+namespace ei_infrastructure.Data.Queries
+{
+    public class GetAllWireColors
+    {
+        /// <summary>
+        ///     Query to return all existing wire colors, or an empty sequence if none exist.
+        /// </summary>
+        public class Query : IRequest<IEnumerable<WireColor>>
+        {
+        }
+
+        public class GetAllWireColorsHandler : IRequestHandler<Query, IEnumerable<WireColor>>
+        {
+            private readonly IDbConnection _dbConnection;
+            private readonly IMapper _mapper;
+
+            public GetAllWireColorsHandler(IDbConnection dbConnection, IMapper mapper)
+            {
+                _dbConnection = dbConnection;
+                _mapper = mapper;
+            }
+
+            public async Task<IEnumerable<WireColor>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var wireColorPocos = (await _dbConnection.GetAllAsync<POCOs.WireColor>()).ToList();
+                return _mapper.Map<IEnumerable<WireColor>>(wireColorPocos);
+            }
+        }
+    }
+}
