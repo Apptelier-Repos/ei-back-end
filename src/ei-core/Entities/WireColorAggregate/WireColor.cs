34576d95390ebfad1078d793b0f780d1a3f432f8
Lesair Valmont <ixra.net@gmail.com>
2019-09-30 15:29:10 -0500

    [AB#178] WireColor aggregate.
    Completing class.

--- src/ei-core/Entities/WireColorAggregate/WireColor.cs
+++ src/ei-core/Entities/WireColorAggregate/WireColor.cs
@@ -5,21 +5,43 @@ namespace ei_core.Entities.WireColorAggregate
 {
     public sealed class WireColor : BaseEntity
     {
-        public string Code { get; }
-        public string Name { get; }
-        public string TranslatedName { get; }
-        public RgbColors Colors { get; }
-
-        public WireColor(int id, string code, string name, string translatedName, RgbColors colors) : base(id)
+        public WireColor(int id, string code, string name, string translatedName, Color baseColor) : base(id)
         {
             Guard.Against.Zero(id, nameof(id));
             Guard.Against.NullOrEmpty(code, nameof(code));
+            Guard.Against.NullOrEmpty(name, nameof(name));
             Guard.Against.NullOrEmpty(translatedName, nameof(translatedName));
 
             Code = code;
             Name = name;
             TranslatedName = translatedName;
-            Colors = colors;
+            BaseColor = baseColor;
+        }
+
+        public WireColor(int id, string code, string name, string translatedName, Color baseColor,
+            Color stripeColor) : base(id)
+        {
+            Guard.Against.Zero(id, nameof(id));
+            Guard.Against.NullOrEmpty(code, nameof(code));
+            Guard.Against.NullOrEmpty(name, nameof(name));
+            Guard.Against.NullOrEmpty(translatedName, nameof(translatedName));
+
+            Code = code;
+            Name = name;
+            TranslatedName = translatedName;
+            BaseColor = baseColor;
+            StripeColor = stripeColor;
+        }
+
+        public string Code { get; }
+        public string Name { get; }
+        public string TranslatedName { get; }
+        public Color BaseColor { get; set; }
+        public Color? StripeColor { get; }
+
+        public override string ToString()
+        {
+            return $"({Id}) {Code}";
         }
     }
 }
