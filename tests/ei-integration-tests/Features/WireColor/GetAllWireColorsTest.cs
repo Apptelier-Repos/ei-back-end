5f7a6dce6b00a7a467f63b742d1aa2c649065fc3
Lesair Valmont <ixra.net@gmail.com>
2019-09-30 18:17:04 -0500

    [AB#196] GetAllWireColors integration tests.

---
+++ tests/ei-integration-tests/Features/WireColor/GetAllWireColorsTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ei_infrastructure.Data.Queries;
+using Shouldly;
+using Xunit;
+using static ei_integration_tests.SliceFixture;
+
+namespace ei_integration_tests.Features.WireColor
+{
+    public class GetAllWireColorsTest : IntegrationTestBase
+    {
+        [Fact]
+        public async void ReturnsAllExistingWireColors()
+        {
+            var wireColors = new List<ei_infrastructure.Data.POCOs.WireColor>
+            {
+                new ei_infrastructure.Data.POCOs.WireColor
+                    {Code = "L", Name = "Blue", TranslatedName = "Azul", BaseColor = "#0000ff"}
+            };
+
+            await InsertAsync(wireColors);
+
+            var query = new GetAllUserAccounts.Query();
+            var result = (await SendAsync(query)).ToList();
+
+            result.ShouldNotBeNull();
+            result.ShouldNotBeEmpty();
+            result.Count.ShouldBeGreaterThanOrEqualTo(1);
+        }
+    }
+}
