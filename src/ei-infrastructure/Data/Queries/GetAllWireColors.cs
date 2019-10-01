e7d70aecfe3a8e0569617f36d249e7b3732cf4f6
Lesair Valmont <ixra.net@gmail.com>
2019-10-01 10:56:30 -0500

    [AB#195] GetAllWireColors converters.
    Using the ColorTranslator.

--- src/ei-infrastructure/Data/Queries/GetAllWireColors.cs
+++ src/ei-infrastructure/Data/Queries/GetAllWireColors.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -12,6 +13,35 @@ namespace ei_infrastructure.Data.Queries
 {
     public class GetAllWireColors
     {
+        public class MappingProfile : Profile
+        {
+            public MappingProfile()
+            {
+                CreateMap<string, Color>().ConvertUsing(new ColorTypeConverter());
+                CreateMap<string, Color?>().ConvertUsing(new NullableColorTypeConverter());
+                CreateMap<POCOs.WireColor, WireColor>();
+                //CreateMap<POCOs.WireColor, WireColor>()
+                //    .ForMember(dest => dest.BaseColor,
+                //        opt => opt.MapFrom(src => ColorTranslator.FromHtml(src.BaseColor)))
+                //    .ForMember(dest => dest.StripeColor,
+                //        opt => opt.MapFrom(src => ColorTranslator.FromHtml(src.BaseColor)));
+            }
+        }
+
+        public class NullableColorTypeConverter : ITypeConverter<string, Color?>
+        {
+            public Color? Convert(string source, Color? destination, ResolutionContext context)
+            {
+                return !string.IsNullOrEmpty(source) ? ColorTranslator.FromHtml(source) : (Color?) null;
+            }
+        }
+
+        public class ColorTypeConverter : ITypeConverter<string, Color>
+        {
+            public Color Convert(string source, Color destination, ResolutionContext context) =>
+                ColorTranslator.FromHtml(source);
+        }
+
         /// <summary>
         ///     Query to return all existing wire colors, or an empty sequence if none exist.
         /// </summary>
@@ -19,7 +49,8 @@ namespace ei_infrastructure.Data.Queries
         {
         }
 
-        public class GetAllWireColorsHandler : IRequestHandler<Query, IEnumerable<WireColor>>
+        public class
+            GetAllWireColorsHandler : IRequestHandler<Query, IEnumerable<WireColor>>
         {
             private readonly IDbConnection _dbConnection;
             private readonly IMapper _mapper;
@@ -30,7 +61,8 @@ namespace ei_infrastructure.Data.Queries
                 _mapper = mapper;
             }
 
-            public async Task<IEnumerable<WireColor>> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<IEnumerable<WireColor>> Handle(Query request,
+                CancellationToken cancellationToken)
             {
                 var wireColorPocos = (await _dbConnection.GetAllAsync<POCOs.WireColor>()).ToList();
                 return _mapper.Map<IEnumerable<WireColor>>(wireColorPocos);
