539e4bffd6801e53b9e26822909baddc043a5437
Lesair Valmont <ixra.net@gmail.com>
2019-10-02 22:51:16 -0500

    [AB#196] Finishing integration tests.
    Assertions and other stuff.

--- tests/ei-integration-tests/Features/WireColor/GetAllWireColorsTest.cs
+++ tests/ei-integration-tests/Features/WireColor/GetAllWireColorsTest.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using ei_infrastructure.Data.Queries;
 using Shouldly;
 using Xunit;
@@ -12,12 +11,17 @@ namespace ei_integration_tests.Features.WireColor
         [Fact]
         public async void ReturnsAllExistingWireColors()
         {
-            var wireColors = new List<ei_infrastructure.Data.POCOs.WireColor>
+            var wireColors = new[]
             {
                 new ei_infrastructure.Data.POCOs.WireColor
-                    {Code = "L", Name = "Blue", TranslatedName = "Azul", BaseColor = "#0000FF"},
+                {
+                    Code = "L", Name = "Blue", TranslatedName = "Azul", BaseColor = "#0000FF"
+                },
                 new ei_infrastructure.Data.POCOs.WireColor
-                    {Code = "LA-L", Name = "Lavender-Blue", TranslatedName = "Lavanda-Azul", BaseColor = "#0000FF", StripeColor = "#B57EDC"}
+                {
+                    Code = "LA-L", Name = "Lavender-Blue", TranslatedName = "Lavanda-Azul", BaseColor = "#0000FF",
+                    StripeColor = "#B57EDC"
+                }
             };
 
             await InsertAsync(wireColors);
@@ -27,7 +31,9 @@ namespace ei_integration_tests.Features.WireColor
 
             result.ShouldNotBeNull();
             result.ShouldNotBeEmpty();
-            result.Count.ShouldBeGreaterThanOrEqualTo(1);
+            result.Count.ShouldBeGreaterThanOrEqualTo(wireColors.Length);
+            result.ShouldContain(wireColor => wireColor.Code == wireColors[0].Code);
+            result.ShouldContain(wireColor => wireColor.Code == wireColors[1].Code);
         }
     }
 }
