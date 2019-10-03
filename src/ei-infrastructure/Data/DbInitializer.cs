1e75b9c7f5a7325f8e8abd5443d0cc84c4d64614
Lesair Valmont <ixra.net@gmail.com>
2019-10-03 06:29:52 -0500

    [AB#194] Preconfigured wire colors.
    Catalog:
    https://dev.azure.com/Apptelier/513c3079-6d3d-4f51-88c8-0a4df6fe5f07/_apis/git/repositories/00f23404-637d-45e9-ad02-6f58b01a6429/Items?path=%2F.attachments%2FColors%20catalog-2883ad1d-2599-4162-902b-4111349ae4d7.xlsx&download=false&resolveLfs=true&%24format=octetStream&api-version=5.0-preview.1

--- src/ei-infrastructure/Data/DbInitializer.cs
+++ src/ei-infrastructure/Data/DbInitializer.cs
@@ -24,6 +24,8 @@ namespace ei_infrastructure.Data
         {
             if (!dbConnection.GetAll<UserAccount>().Any())
                 await dbConnection.InsertAsync(GetPreconfiguredUserAccounts());
+            if (!dbConnection.GetAll<WireColor>().Any())
+                await dbConnection.InsertAsync(GetPreconfiguredWireColors());
         }
 
         private static IEnumerable<UserAccount> GetPreconfiguredUserAccounts()
@@ -34,6 +36,42 @@ namespace ei_infrastructure.Data
                 new UserAccount {Username = "rios", Password = "loerardo123"},
                 new UserAccount {Username = "mota", Password = "maciaseo123"},
                 new UserAccount {Username = "lesair", Password = "israel123"}
+            };
+        }
+
+        private static IEnumerable<WireColor> GetPreconfiguredWireColors()
+        {
+            return new[]
+            {
+                new WireColor {Code = "L", Name = "Blue", TranslatedName = "Azul", BaseColor = "#0000FF"},
+                new WireColor {Code = "O", Name = "Orange", TranslatedName = "Naranja", BaseColor = "#FFA500"},
+                new WireColor {Code = "B", Name = "Black", TranslatedName = "Negro", BaseColor = "#000000"},
+                new WireColor {Code = "R", Name = "Red", TranslatedName = "Rojo", BaseColor = "#FF0000"},
+                new WireColor {Code = "BR", Name = "Brown", TranslatedName = "Café", BaseColor = "#A52A2A"},
+                new WireColor {Code = "G", Name = "Green", TranslatedName = "Verde", BaseColor = "#008000"},
+                new WireColor {Code = "Y", Name = "Yellow", TranslatedName = "Amarillo", BaseColor = "#FFFF00"},
+                new WireColor {Code = "PU", Name = "Purple", TranslatedName = "Púrpura", BaseColor = "#800080"},
+                new WireColor {Code = "SB", Name = "Sky Blue", TranslatedName = "Azul Cielo", BaseColor = "#87CEEB"},
+                new WireColor {Code = "W", Name = "White", TranslatedName = "Blanco", BaseColor = "#FFFFFF"},
+                new WireColor {Code = "LG", Name = "Light Green", TranslatedName = "Verde Claro", BaseColor = "#90EE90"},
+                new WireColor {Code = "GR", Name = "Gray", TranslatedName = "Gris", BaseColor = "#808080"},
+                new WireColor {Code = "P", Name = "Pink", TranslatedName = "Pink", BaseColor = "#FFC0CB"},
+                new WireColor {Code = "BG", Name = "Beige", TranslatedName = "Beige", BaseColor = "#F5F5DC"},
+                new WireColor {Code = "LA", Name = "Lavender", TranslatedName = "Lavanda", BaseColor = "#E6E6FA"},
+                new WireColor {Code = "LA-L", Name = "Lavender-Blue", TranslatedName = "Lavanda-Azul", BaseColor = "#E6E6FA", StripeColor = "#0000FF"},
+                new WireColor {Code = "LA-O", Name = "Lavender-Orange", TranslatedName = "Lavanda-Naranja", BaseColor = "#E6E6FA", StripeColor = "#FFA500"},
+                new WireColor {Code = "LA-B", Name = "Lavender-Black", TranslatedName = "Lavanda-Negro", BaseColor = "#E6E6FA", StripeColor = "#000000"},
+                new WireColor {Code = "LA-R", Name = "Lavender-Red", TranslatedName = "Lavanda-Rojo", BaseColor = "#E6E6FA", StripeColor = "#FF0000"},
+                new WireColor {Code = "LA-BR", Name = "Lavender-Brown", TranslatedName = "Lavanda-Café", BaseColor = "#E6E6FA", StripeColor = "#A52A2A"},
+                new WireColor {Code = "LA-G", Name = "Lavender-Green", TranslatedName = "Lavanda-Verde", BaseColor = "#E6E6FA", StripeColor = "#008000"},
+                new WireColor {Code = "LA-Y", Name = "Lavender-Yellow", TranslatedName = "Lavanda-Amarillo", BaseColor = "#E6E6FA", StripeColor = "#FFFF00"},
+                new WireColor {Code = "LA-PU", Name = "Lavender-Purple", TranslatedName = "Lavanda-Púrpura", BaseColor = "#E6E6FA", StripeColor = "#800080"},
+                new WireColor {Code = "LA-SB", Name = "Lavender-Sky Blue", TranslatedName = "Lavanda-Azul Cielo", BaseColor = "#E6E6FA", StripeColor = "#87CEEB"},
+                new WireColor {Code = "LA-W", Name = "Lavender-White", TranslatedName = "Lavanda-Blanco", BaseColor = "#E6E6FA", StripeColor = "#FFFFFF"},
+                new WireColor {Code = "LA-LG", Name = "Lavender-Light Green", TranslatedName = "Lavanda-Verde Claro", BaseColor = "#E6E6FA", StripeColor = "#90EE90"},
+                new WireColor {Code = "LA-GR", Name = "Lavender-Gray", TranslatedName = "Lavanda-Gris", BaseColor = "#E6E6FA", StripeColor = "#808080"},
+                new WireColor {Code = "LA-P", Name = "Lavender-Pink", TranslatedName = "Lavanda-Rosa", BaseColor = "#E6E6FA", StripeColor = "#FFC0CB"},
+                new WireColor {Code = "LA-BG", Name = "Lavender-Beige", TranslatedName = "Lavanda-Beige", BaseColor = "#E6E6FA", StripeColor = "#F5F5DC"}
             };
         }
     }
