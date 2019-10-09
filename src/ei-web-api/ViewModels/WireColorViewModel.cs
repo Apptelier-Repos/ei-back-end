6217137154000cf7087effa20fdd442d37bddb89
Lesair Valmont <ixra.net@gmail.com>
2019-10-08 23:57:56 -0500

    [AB#202] WireColorViewModel.

---
+++ src/ei-web-api/ViewModels/WireColorViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ei_web_api.ViewModels
+{
+    public class WireColorViewModel
+    {
+        public int Id { get; set; }
+        public string Code { get; set;  }
+        public string Name { get; set; }
+        public string TranslatedName { get; set; }
+        public string BaseWebColor { get; set; }
+        public string StripeWebColor { get; set; }
+    }
+}
