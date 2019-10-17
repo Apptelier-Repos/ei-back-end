40a48cc8dda3f728783d91f9961cdedd28f035a7
Lesair Valmont <ixra.net@gmail.com>
2019-10-17 18:02:44 -0500

    [AB#206] ei-functional-tests project creation.

---
+++ tests/ei-functional-tests/Web/CustomWebApplicationFactory.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using ei_slice;
+using ei_web_api;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ei_functional_tests.Web
+{
+    public class CustomWebApplicationFactory : WebApplicationFactory<Startup>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder webHostBuilder)
+        {
+            webHostBuilder.ConfigureAppConfiguration((context, builder) =>
+            {
+                builder.SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", true, true)
+                    .AddEnvironmentVariables();
+            });
+
+            webHostBuilder.ConfigureServices(async services =>
+            {
+                var provider = services.BuildServiceProvider();
+
+                using (provider.CreateScope())
+                {
+                    await Initialize.DatabaseAsync();
+                }
+            });
+        }
+    }
+}
