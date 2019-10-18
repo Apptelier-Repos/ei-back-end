83c948647897a4b55fea3f1bdaaa910a61fe58f4
Lesair Valmont <ixra.net@gmail.com>
2019-10-18 10:42:42 -0500

    [AB#212] Improvements to GetAWireColorByCodeTest.
    Naming improvements and usage of random strings.

--- tests/ei-integration-tests/Features/WireColor/GetAWireColorByCodeTest.cs
+++ tests/ei-integration-tests/Features/WireColor/GetAWireColorByCodeTest.cs
@@ -2,6 +2,7 @@
 using Shouldly;
 using Xunit;
 using static ei_slice.Fixture;
+using static ei_utils.StringUtils;
 
 // ReSharper disable StringLiteralTypo
 
@@ -10,13 +11,46 @@ namespace ei_integration_tests.Features.WireColor
     public class GetAWireColorByCodeTest : IntegrationTestBase
     {
         [Fact]
-        public async void ReturnsAWireColorWhenThereIsAMatch()
+        public async void ReturnsAWireColorWithCustomBaseColorAndNoStripeColor()
         {
-            const string orangeCode = "O";
-            const string orangeName = "Orange";
-            const string orangeTranslatedName = "Naranja";
-            const string orangeColorKnownName = "Orange";
+            const string customCode = "X3";
+            const string customName = "Custom color 3";
+            const string customTranslatedName = "Color personalizado 3";
+            const string customColorHexTriplet = "#ABCDEF";
+            const string customColorName = "ffabcdef";
+            var wireColors = new[]
+            {
+                new ei_infrastructure.Data.POCOs.WireColor
+                {
+                    Code = customCode, Name = customName, TranslatedName = customTranslatedName,
+                    BaseColor = customColorHexTriplet
+                }
+            };
+            await InsertAsync(wireColors);
+
+            var query = new GetAWireColorByCode.Query(customCode);
+            var result = await SendAsync(query);
+
+            result.ShouldNotBeNull();
+            result.Id.ShouldBeGreaterThan(0);
+            result.Code.ShouldBe(customCode);
+            result.Name.ShouldBe(customName);
+            result.TranslatedName.ShouldBe(customTranslatedName);
+            result.BaseColor.Name.ShouldBe(customColorName);
+            result.BaseColor.IsKnownColor.ShouldBeFalse();
+            result.StripeColor.HasValue.ShouldBeFalse();
+        }
+
+        [Fact]
+        public async void ReturnsAWireColorWithKnownBaseAndStripeColors()
+        {
+            const string lavenderOrangeCode = "LA-O";
+            const string lavenderOrangeName = "Lavender-Orange";
+            const string lavenderOrangeTranslatedName = "Lavanda-Naranja";
+            const string lavenderColorHexTriplet = "#E6E6FA";
+            const string lavenderColorName = "Lavender";
             const string orangeColorHexTriplet = "#FFA500";
+            const string orangeColorName = "Orange";
             var wireColors = new[]
             {
                 new ei_infrastructure.Data.POCOs.WireColor
@@ -25,32 +59,37 @@ namespace ei_integration_tests.Features.WireColor
                 },
                 new ei_infrastructure.Data.POCOs.WireColor
                 {
-                    Code = orangeCode, Name = orangeName, TranslatedName = orangeTranslatedName,
+                    Code = "O", Name = "Orange", TranslatedName = "Naranja",
                     BaseColor = orangeColorHexTriplet
                 },
                 new ei_infrastructure.Data.POCOs.WireColor
                 {
-                    Code = "LA-O", Name = "Lavender-Orange", TranslatedName = "Lavanda-Naranja", BaseColor = "#E6E6FA",
+                    Code = lavenderOrangeCode, Name = lavenderOrangeName, TranslatedName = lavenderOrangeTranslatedName,
+                    BaseColor = lavenderColorHexTriplet,
                     StripeColor = orangeColorHexTriplet
                 }
             };
             await InsertAsync(wireColors);
 
-            var query = new GetAWireColorByCode.Query(orangeCode);
+            var query = new GetAWireColorByCode.Query(lavenderOrangeCode);
             var result = await SendAsync(query);
 
             result.ShouldNotBeNull();
             result.Id.ShouldBeGreaterThan(0);
-            result.Code.ShouldBe(orangeCode);
-            result.Name.ShouldBe(orangeName);
-            result.TranslatedName.ShouldBe(orangeTranslatedName);
-            result.BaseColor.Name.ShouldBe(orangeColorKnownName);
+            result.Code.ShouldBe(lavenderOrangeCode);
+            result.Name.ShouldBe(lavenderOrangeName);
+            result.TranslatedName.ShouldBe(lavenderOrangeTranslatedName);
+            result.BaseColor.Name.ShouldBe(lavenderColorName);
+            result.BaseColor.IsKnownColor.ShouldBeTrue();
+            result.StripeColor.HasValue.ShouldBeTrue();
+            result.StripeColor?.Name.ShouldBe(orangeColorName);
+            result.StripeColor?.IsKnownColor.ShouldBeTrue();
         }
 
         [Fact]
         public async void ReturnsNullWhenThereAreNoMatches()
         {
-            const string pinkCode = "P";
+            var randomCode = RandomString(5);
             var wireColor = new ei_infrastructure.Data.POCOs.WireColor
             {
                 Code = "LA-P",
@@ -61,7 +100,7 @@ namespace ei_integration_tests.Features.WireColor
             };
             await InsertAsync(wireColor);
 
-            var query = new GetAWireColorByCode.Query(pinkCode);
+            var query = new GetAWireColorByCode.Query(randomCode);
             var result = await SendAsync(query);
 
             result.ShouldBeNull();
